3b3bd6cda0a0d171e6594513b1544769f3aa3ecb
hecheng.cy <hecheng.cy@xuelangyun.com>
2022-04-21 15:10:38 +0800

    add description

--- generate.js
+++ generate.js
@@ -1,5 +1,6 @@
 const compiler = require('@vue/compiler-core')
 const fs = require('fs')
+// this used to parse template
 fs.readFile('./src/components/HelloWorld.vue', 'utf-8', (err,data) => {
     // console.log(err,data)
     if(err) {
@@ -7,9 +8,15 @@ fs.readFile('./src/components/HelloWorld.vue', 'utf-8', (err,data) => {
         return;
     } else {
         const match = data.match(/\<template\>([\s\S]*)\<\/template\>/);
-        console.log(compiler.baseParse(match[0]));
+        if(match) {
+            // 考虑fork@vue/compiler-core
+            const ast = compiler.baseParse(match[0]);
+        }
         // 这里就是ast树了 
     }
 })
 
-// console.log(compiler.baseParse('<div>hi</div>'))
+// try to parse tsx
+// 这里看需求,目前vue3 基本使用vue-class-component来实现class风格api,是否需要做待定
+
+
